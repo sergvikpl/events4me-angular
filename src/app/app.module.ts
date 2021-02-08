aacb36ebbbca96853fe18f6d8082261df07a0859
sergvikpl <sergvikpl@mail.ru>
2021-02-08 23:38:11 +0300

    Added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {Routes, RouterModule } from '@angular/router';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SiteHeaderComponent } from './components/site-header/site-header.component';
 import { SiteFooterComponent } from './components/site-footer/site-footer.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SiteHeaderComponent,
-    SiteFooterComponent
+    SiteFooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule,
     FontAwesomeModule
   ],
