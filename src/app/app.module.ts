5c22c88f94fef1b95bb7b81bb6a74395a07cf8d2
sergvikpl <sergvikpl@mail.ru>
2021-02-09 13:16:27 +0300

    Added validation for login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SiteHeaderComponent } from './components/site-header/site-header.component';
 import { SiteFooterComponent } from './components/site-footer/site-footer.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+
 import { HomeComponent } from './pages/home/home.component';
 import { AgreementComponent } from './pages/agreement/agreement.component';
-import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { LoginComponent } from './pages/login/login.component';
 
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +32,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from '../../auth.service';
 
 @Component({
@@ -8,16 +9,32 @@ import { AuthService } from '../../auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  form: FormGroup
+
   constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required]), // Validators.email
+      password: new FormControl(null, [Validators.required])
+    })
   }
 
   onSubmit() {
-    this.auth.login(
-      {
-        email: 'test',
-        password: 'test'
+    this.form.disable()
+
+    const user = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+
+    this.auth.login(user).subscribe(
+      () => {
+        console.log('Login success')
+      },
+      (error) => {
+        console.warn('Login faild', error)
+        this.form.enable()
       }
     )
   }
